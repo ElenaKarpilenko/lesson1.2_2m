58245dcbd621830ec647f1d822e0689fcc23c883
elenakarpilenko <elenakarpilenko787@gmail.com>
2024-07-04 19:19:06 +0600

    .

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,21 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello world!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        String[] commands = {"Sit", "Run"};
+        Shelter shelter = new Shelter("Peace", "Lenina 1");
+
+        final Dog dog = new Dog("Rex", "Pitbul", commands, shelter,
+                ColorEnum.BROWN);
+
+        Dog dog2 = new Dog("Layka", "Senbernar", shelter, ColorEnum.GREY);
+
+
+        System.out.println(dog);
+        dog.makeVoice();
+        dog.makeVoice("Гив гив");
+        dog.makeVoice("Гув гув");
+        System.out.println("----------------");
+        System.out.println(dog2);
     }
 }
